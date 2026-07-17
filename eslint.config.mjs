61523679bffe88967c17b2090edd0de4c6a2d9ca
ashtonckj <stickmangamer0730@gmail.com>
2026-07-17 15:53:18 +0800

    refactor: changes typescript esline import

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,33 @@
-import typescriptEslint from "typescript-eslint";
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
 
-export default [{
-    files: ["**/*.ts"],
-}, {
-    plugins: {
-        "@typescript-eslint": typescriptEslint.plugin,
+// Flat config. Uses the installed @typescript-eslint/{parser,eslint-plugin}
+// packages directly (the previous config imported the `typescript-eslint`
+// meta-package, which is not a dependency of this project).
+export default [
+    {
+        files: ["**/*.ts"],
+
+        plugins: {
+            "@typescript-eslint": tsPlugin,
+        },
+
+        languageOptions: {
+            parser: tsParser,
+            ecmaVersion: 2022,
+            sourceType: "module",
+        },
+
+        rules: {
+            "@typescript-eslint/naming-convention": ["warn", {
+                selector: "import",
+                format: ["camelCase", "PascalCase"],
+            }],
+
+            // curly: "warn",
+            eqeqeq: "warn",
+            "no-throw-literal": "warn",
+            semi: "warn",
+        },
     },
-
-    languageOptions: {
-        parser: typescriptEslint.parser,
-        ecmaVersion: 2022,
-        sourceType: "module",
-    },
-
-    rules: {
-        "@typescript-eslint/naming-convention": ["warn", {
-            selector: "import",
-            format: ["camelCase", "PascalCase"],
-        }],
-
-        curly: "warn",
-        eqeqeq: "warn",
-        "no-throw-literal": "warn",
-        semi: "warn",
-    },
-}];
+];
